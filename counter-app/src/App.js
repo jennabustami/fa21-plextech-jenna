2f767a94dfd9183fbc389421e5ccd4e65e3f9449
Jenna Bustami <jennabustami@berkeley.edu>
2021-10-28 12:15:37 -0700

    button

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,11 +1,39 @@
 import './App.css';
+import React, { Component } from 'react';
+import Button from './Button.js'; 
 
-function App() {
+export default class App extends Component {
+  constructor(){
+      super();
+      this.state= {
+            count: 0
+        };
+}
+// Main Solution
+incrementCount = () => {
+    this.setState({
+        count: this.state.count + 1
+  });
+}
+decrementCount = () => {
+  this.setState({
+      count: this.state.count - 1
+    });
+}
+render () {
+  let { count } = this.state;
   return (
-    <div className="App">
-      
-    </div>
+    <div>
+<h2>Count: { count } </h2>
+<Button
+title = { "+" }
+task = { () => this.incrementCount() }
+/>
+<Button
+title = { "-" }
+task = { () => this.decrementCount() }
+/>
+</div>
   );
 }
-
-export default App;
+}
