c97a6b19601085dd02c54bd07b812b90159a7552
Jenna Bustami <jennabustami@berkeley.edu>
2021-11-19 08:12:03 -0800

    update tasks-api

--- tasks-api/src/server.js
+++ tasks-api/src/server.js
@@ -7,4 +7,16 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
+app.post("/v1/tasks", (req, res) => {
+    res.json("Hello World");
+});
+
+app.get("/v1/tasks", (req, res) => {
+    res.send("Hello World");
+});
+
+app.get("/v1/tasks{id}", (req, res) => {
+    res.send(req.params);
+});
+
 app.listen(port, () => console.log(`The server is listening on port ${port}`))
