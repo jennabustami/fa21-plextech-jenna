476cf258110371f3eb9a33417fb99d7d8c9942b3
Jenna Bustami <jennabustami@berkeley.edu>
2021-11-20 08:14:23 -0800

    finished

--- tasks-api/src/server.js
+++ tasks-api/src/server.js
@@ -2,21 +2,63 @@ const express = require("express");
 
 const app = express();
 const port = 8000;
+const tasks = require("./tasks");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
 app.post("/v1/tasks", (req, res) => {
-    res.json("Hello World");
+    const newTask = {
+        title: req.body.title,
+        is_completed: false,
+    }
+
+    tasks.push(newTask);
+    res.json(tasks.id);
 });
 
 app.get("/v1/tasks", (req, res) => {
-    res.send("Hello World");
+    res.json(tasks);
 });
 
-app.get("/v1/tasks{id}", (req, res) => {
-    res.send(req.params);
+app.get("/v1/tasks:id", (req, res) => {
+    const found = tasks.some(task => task.id === parseInt(req.params.id));
+
+    if(found){
+    res.json(tasks.filter(task=> task.id === parseInt(req.params.id)));
+    } else {
+        res.status(404).json({error: "There is no task at that id"})
+    }
 });
+
+app.delete("/v1/tasks:id", (req, res) => {
+    const found = tasks.some(task => task.id === parseInt(req.params.id));
+
+    if(found){
+    res.json(tasks.filter(task=> task.id !== parseInt(req.params.id)));
+    } 
+    res.status(204)
+});
+
+app.put("/v1/tasks:id", (req, res) => {
+    const found = tasks.some(task => task.id === parseInt(req.params.id));
+   if(found){
+    tasks.forEach(task=> {
+        if(task.id===parseInt(req.params.id)){
+            task.is_completed = req.body.is_completed;
+            task.title = req.body.title;
+        }
+    })   
+    res.status(204)
+   }
+    else {
+        res.status(404).json({error: "There is no task at that id"})
+    }
+});
+
 
 app.listen(port, () => console.log(`The server is listening on port ${port}`))
---
+++ tasks-api/src/tasks.js
@@ -0,0 +1,18 @@
+const tasks = [
+        {
+            id: 1,
+            title: "Test Task 1",
+            is_completed: true
+        },
+        {
+            id: 2,
+            title: "Test Task 2",
+            is_completed: false
+            },
+        {
+            id: 3,
+            title: "Test Task 3",
+            is_completed: true
+            }
+        ];
+module.exports = tasks;
